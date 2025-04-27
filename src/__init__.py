502d99cc4de4f4b3979b93fc8d70c0bdf3bc4b02
Francehh <120748110+Francehhh@users.noreply.github.com>
2025-04-27 16:22:04 +0200

    Delete files

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-PsW - Password Manager
-Autore: Francehh
-
-Un'applicazione desktop moderna e sicura per la gestione delle credenziali degli utenti.
-"""
-
-__version__ = "Alpha 1.4.3"
-__author__ = "Francehh" 
